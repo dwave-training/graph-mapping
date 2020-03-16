7f9eb1cd567c1dd2029b1e0ccfcfb11864d13b64
Victoria Goliber <vgoliber@dwavesys.com>
2020-03-16 09:01:42 +0900

    Add change problem exercise file

---
+++ change-problem.py
@@ -0,0 +1,67 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Import networkx for graph tools
+import networkx as nx
+
+# Import dwave_networkx for d-wave graph tools/functions
+import dwave_networkx as dnx
+
+# Import matplotlib.pyplot to draw graphs on screen
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+# Set the solver we're going to use
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+
+sampler = EmbeddingComposite(DWaveSampler(solver={'qpu': True}))
+
+# Create empty graph
+G = nx.Graph()
+
+# Add edges to graph - this also adds the nodes
+
+## TODO:  Update graph to new problem graph
+
+# Find the maximum independent set, S
+
+
+## TODO:  Update dwave-networkx function to new problem function
+
+# Print the solution for the user
+print('Minimum vertex cover size found is', len(S))
+print(S)
+
+# Visualize the results
+k = G.subgraph(S)
+notS = list(set(G.nodes()) - set(S))
+othersubgraph = G.subgraph(notS)
+pos = nx.spring_layout(G)
+plt.figure()
+
+# Save original problem graph
+original_name = "vertex_cover_original.png"
+nx.draw_networkx(G, pos=pos, with_labels=True)
+plt.savefig(original_name, bbox_inches='tight')
+
+# Save solution graph
+# Note: red nodes are in the set, blue nodes are not
+solution_name = "vertex_cover_solution.png"
+nx.draw_networkx(k, pos=pos, with_labels=True, node_color='r', font_color='k')
+nx.draw_networkx(othersubgraph, pos=pos, with_labels=True, node_color='b', font_color='w')
+plt.savefig(solution_name, bbox_inches='tight')
+
+print("Your plots are saved to {} and {}".format(original_name, solution_name))
