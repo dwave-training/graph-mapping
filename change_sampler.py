0d0a16edc3e9bbfcee7b1341c6aa673e6d22e958
Victoria Goliber <vgoliber@dwavesys.com>
2020-04-21 10:45:58 +0900

    Add solve_problem function

--- change_sampler.py
+++ change_sampler.py
@@ -36,6 +36,13 @@ def create_graph():
 
     return G
 
+def solve_problem(G, sampler):
+
+    # Find the maximum independent set, S
+    S = dnx.maximum_independent_set(G, sampler=sampler, num_reads=10)
+
+    return S
+
 ## ------- Main program -------
 if __name__ == "__main__":
 
@@ -43,8 +50,7 @@ if __name__ == "__main__":
 
     sampler = set_sampler()
 
-    # Find the maximum independent set, S
-    S = dnx.maximum_independent_set(G, sampler=sampler, num_reads=10)
+    S = solve_problem(G, sampler)
 
     # Print the solution for the user
     print('Maximum independent set size found is', len(S))
