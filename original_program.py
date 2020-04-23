73a752033102febd920d611ae1b96ec2d58e16ad
Victoria Goliber <vgoliber@dwavesys.com>
2020-04-23 16:11:09 +0900

    Fix line break

--- original_program.py
+++ original_program.py
@@ -43,7 +43,8 @@ def create_graph():
     G = nx.Graph()
 
     # Add edges to graph - this also adds the nodes
-    G.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5), (4, 5), (4, 6), (5, 6), (6, 7)])
+    G.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4), (3, 5), 
+                        (4, 5), (4, 6), (5, 6), (6, 7)])
 
     return G
 
